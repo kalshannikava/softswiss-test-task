6f66f562d0e7daa232cc17f7c0b279d05439d8b0
k.alshannikava <olshannikovastw@gmail.com>
2024-04-29 23:39:46 +0200

    Add base markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
 
+import './App.scss';
+import './styles/grid.scss';
+
+import { cards, articles } from './data';
+
+import Header from './components/Header/Header';
+import Banner from './components/Banner/Banner';
+import Footer from './components/Footer/Footer';
+import Card from './components/Card/Card';
+import Article from './components/Article/Article';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        App
-      </header>
+    <div className='app'>
+      <div className='grid-0'>
+        <Header className='col-sm-8 col-md-12 col-lg-12'/>
+        <Banner className='col-sm-8 col-md-12 col-lg-12' />
+      </div>
+      <main className='content grid'>
+        <h2 className='content__title col-sm-8 col-md-12 col-lg-12'>Offers</h2>
+        {cards.map(({ id, title, description, background, classNames, titleSize }) =>
+          <div className={classNames} key={`card_container_${id}`}>
+            <Card title={title} description={description} background={background} titleSize={titleSize} key={`card_${id}`} />
+          </div>)}
+        {articles.map(({ id, visibleText, hiddenText, title, classNames }) =>
+          <Article title={title} visibleText={visibleText} hiddenText={hiddenText} key={id} className={classNames}  />)}
+      </main>
+      <div className='grid-0'>
+        <Footer className='col-sm-8 col-md-12 col-lg-12' />
+      </div>
     </div>
   );
 }
