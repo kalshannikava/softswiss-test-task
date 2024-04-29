c3914b82c99191addf41b8cae7d36d5f643cb9ef
k.alshannikava <olshannikovastw@gmail.com>
2024-04-29 23:39:35 +0200

    Add data

--- src/data.ts
+++ src/data.ts
@@ -3,30 +3,45 @@ import space2 from './assets/images/space2.png';
 import space3 from './assets/images/space3.png';
 import space4 from './assets/images/space4.png';
 
-export const cards = [
+export const cards: {
+  id: number,
+  title: string,
+  description: string,
+  background: string,
+  classNames: string,
+  titleSize: 'large' | 'small',
+}[] = [
   {
     id: 1,
     title: 'Move the borders of reality!',
     description: 'Go on a space adventure',
     background: space1,
+    classNames: 'col-sm-8 col-md-6 col-lg-12',
+    titleSize: 'large',
   },
   {
     id: 2,
     title: 'Space is not just stars and planets',
     description: 'it is a majestic journey to',
     background: space2,
+    classNames: 'col-sm-8 col-md-6 col-lg-6',
+    titleSize: 'small',
   },
   {
     id: 3,
     title: 'For those who dream of stars',
     description: 'Our offer: make your dream come true',
     background: space3,
+    classNames: 'col-sm-8 col-md-6 col-lg-6',
+    titleSize: 'small',
   },
   {
     id: 4,
     title: 'Fulfill your fantastic dreams',
     description: 'Space has never been so close',
     background: space4,
+    classNames: 'col-sm-8 col-md-6 col-lg-12',
+    titleSize: 'large',
   },
 ];
 
@@ -36,5 +51,6 @@ export const articles = [
     visibleText: 'Traveling into space is one of the most exciting and unforgettable adventures that can change your life forever. And if you have ever dreamed of exploring stars, planets and galaxies, then our company is ready to help you realize this dream. We offer a unique experience that will allow you to go on a space journey and see all the secrets of the universe. We guarantee that every moment in space will be filled with incredible impressions, excitement and new discoveries. Our team of professionals takes care of your safety and comfort so that you can fully enjoy your adventure in space. We offer various options for space excursions.',
     hiddenText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     title: 'Embark on a space journey',
+    classNames: 'col-sm-8 col-md-12 col-lg-12',
   }
 ];
